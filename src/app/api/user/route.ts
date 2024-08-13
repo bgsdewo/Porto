2ecc0afde60e74415fb262332844c6f6e1f1dada
unknown <bgsdewoo20@gmail.com>
2024-08-13 10:48:06 +0700

    Response Helper

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,41 +1,31 @@
+import Response from "@/lib/api.response";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json(
-    {
-      success: true,
-      message: "Get all users",
-      data: [
-        {
-          id: 1,
-          name: "Taufan",
-        },
-        {
-          id: 2,
-          name: "Fadhilah",
-        },
-      ],
-    },
-    {
-      status: 200,
-    }
-  );
+  return Response({
+    message: "Get all users",
+    data: [
+      {
+        id: 1,
+        name: "Taufan",
+      },
+      {
+        id: 2,
+        name: "Fadhilah",
+      },
+    ],
+    status: 200,
+  });
 }
 
 export async function POST() {
-  return NextResponse.json(
-    {
-      success: true,
-      message: "Get all users",
-      data: [
-        {
-          id: 3,
-          name: "Mas Bagus",
-        },
-      ],
-    },
-    {
-      status: 200,
-    }
-  );
+  return Response({
+    message: "Get all users",
+    data: [
+      {
+        id: 3,
+        name: "Mas Bagus",
+      },
+    ],
+  });
 }
